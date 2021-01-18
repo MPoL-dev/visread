1f949b46fa3194f783305dd109ca259ed76ce848
Ian Czekala <iancze@gmail.com>
2021-01-17 20:09:26 -0500

    try python3 again.

--- docs/tutorials/plot_baselines.py
+++ docs/tutorials/plot_baselines.py
@@ -5,11 +5,11 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.5.1
+#       jupytext_version: 1.6.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
-#     name: python
+#     name: python3
 # ---
 
 # # Quickstart
