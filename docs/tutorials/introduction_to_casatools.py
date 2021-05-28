bace85aea68ec376273163c1b0eddf05b024922c
Ian Czekala <ipc5094@psu.edu>
2021-05-28 10:52:08 -0400

    added note about exporting flags.

--- docs/tutorials/introduction_to_casatools.py
+++ docs/tutorials/introduction_to_casatools.py
@@ -346,6 +346,10 @@ vv = 1e-3 * vv / wavelengths  # [klambda]
 
 frequencies = chan_freq * 1e-9  # [GHz]
 
+# Since we have a multi-channel dataset, we need to make a decision about how to treat flagged visibilities. If we wanted to export only unflagged visibilities, there is a chance that some channels will have different number of flagged visibilities, which means we can no longer represent the data as a nicely packed, contiguous, rectangular array.
+# 
+#  Here, we sidestep this issue by just exporting *all* of the data (including flagged, potentially erroneous, visibilities) but, we also include the ``mask`` used to identify the valid visibilities. This defers the final decision about how to store unflagged visibilities. But the user will need to make sure that they correctly apply the mask when they load the data in a new environment.
+
 # save data to numpy file for later use
 np.savez(
     "visibilities.npz",
