cbeac5d4ca8bdcfa9173f94327245debd67c567c
Ian Czekala <ipc5094@psu.edu>
2022-11-18 16:17:00 -0500

    some routine updates.

--- src/visread/process.py
+++ src/visread/process.py
@@ -19,21 +19,24 @@ def weight_to_sigma(weight):
     return np.sqrt(1 / weight)
 
 
-def broadcast_weights(weight, data_shape, chan_axis=1):
+def broadcast_weights(weight, data_shape, chan_axis=0):
     r"""
-    Broadcast a vector of non-channelized weights to match the shape of the visibility data that is channelized (e.g., for spectral line applications).
+    Broadcast a vector of non-channelized weights to match the shape of the visibility data that is channelized (e.g., for spectral line applications) but already averaged over polarizations.
 
     Args:
         weight (np.array): the weights
         data_shape (tuple): the shape of the data
-        chan_axis (int): which axis represents the number of channels?
+        chan_axis (int): the axis which represents the number of channels in the data array, typically 0 for visibility data that has already been averaged over polarizations.
 
+    Returns:
+        np.array (float) array of weights the same shape as the data
     """
+
 
     nchan = data_shape[chan_axis]
 
-    broadcast = np.ones((2, nchan, 1))
-    return weight[:, np.newaxis, :] * broadcast
+    broadcast = np.ones((nchan, 1))
+    return weight[np.newaxis, :] * broadcast
 
 
 def rescale_weights(weight, sigma_rescale):
@@ -54,12 +57,12 @@ def rescale_weights(weight, sigma_rescale):
     return weight / (sigma_rescale**2)
 
 
-def sum_weight_polarization(weight, polarization_axis=0):
+def average_weight_polarization(weight, polarization_axis=0):
     """
     Average the weights over the polarization axis.
 
     Args:
-        weights (np.array): weight array. Could be shape `(2, nchan, nvis)` or just `(2, nvis)`, dependending on whether it has been broadcasted already. 
+        weight (np.array): weight array. Could be shape `(2, nchan, nvis)` or just `(2, nvis)`, dependending on whether it has been broadcasted already. 
         polarization_axis (int): the polarization axis, typically 0.
 
     Returns:
@@ -67,6 +70,20 @@ def sum_weight_polarization(weight, polarization_axis=0):
     """
 
     return np.sum(weight, axis=polarization_axis)
+
+def average_flag_polarization(flag, polarization_axis=0):
+    """
+    Collapse the flags across the polarization axis, taking the approach that if either polarization is flagged, the averaged product shoud be flagged too.
+
+    Args:
+        flag (np.array bool): flag array. Could be multidimensional, e.g. `(2, nchan, nvis)` or just `(2, nvis)`.
+        polarization_axis (int): the polarization axis, typically 0.
+
+    Returns:
+        (np.array bool): flag array collapsed across the polarization axis. Could be shape `(nchan, nvis)` or just `(nvis)` depending on whether it was broadcasted across channels.
+
+    """
+    return np.any(flag, axis=polarization_axis)
 
 
 def convert_baselines(baselines, freq):
@@ -87,9 +104,9 @@ def convert_baselines(baselines, freq):
     return 1e-3 * baselines / wavelengths  # [klambda]
 
 
-def broadcast_baselines(u, v, chan_freq):
+def broadcast_and_convert_baselines(u, v, chan_freq):
     r"""
-    Convert baselines to kilolambda and broadcast
+    Convert baselines to kilolambda and broadcast to match shape of channel frequencies.
 
     Args:
         u (1D array nvis): baseline [m]
@@ -118,41 +135,92 @@ def broadcast_baselines(u, v, chan_freq):
     return (uu, vv)
 
 
-def average_data_polarizations(data, weight, flag=None, polarization_axis=0):
+def average_data_polarization(data, weight, polarization_axis=0):
     """
-    Perform a weighted average of the data over the polarization axis and propagate flags, if necessary.
+    Perform a weighted average of the data over the polarization axis.
 
     Args:
         data (npol, nchan, nvis): complex data array. Could either be real data or model_data.
-        weight (npol, nchan, nvis): weight array
-        flag (npol, nchan, nvis): bool flag array
+        weight (npol, nvis): weight array matching data array
         polarization_axis (int): index of the polarization axis, typically 0.
 
     Returns:
-        data, flag, weight averaged over the polarization axis each (nchan, nvis)
+        data averaged over the polarization axis.
     """
-    assert data.shape[0] == 2, "Not recognized as a dual-polarization dataset"
+    assert data.shape[polarization_axis] == 2, "Not recognized as a dual-polarization dataset"
 
-    data = np.sum(data * weight, axis=0) / np.sum(weight, axis=0)
+    # we need to check whether weight is the same shape as the data, because sometimes the data is 
+    # channelized and the weights are not
+    # e.g., data would have shape (npol, nchan, nvis)
+    # while weight would have shape (npol, nvis)
 
-    if model_data is not None:
-        model_data = np.sum(model_data * weight, axis=0) / np.sum(weight, axis=0)
+    # normalization after averaging over the polarization axis
+    norm = average_weight_polarization(weight, polarization_axis=polarization_axis)
 
-    flag = np.any(flag, axis=0)
-    weight = np.sum(weight, axis=0)
-
-    if model_data is not None:
-        return data, flag, weight, model_data
+    if len(data.shape) == len(weight.shape):
+        return np.sum(data * weight, axis=polarization_axis) / norm
+    elif (len(data.shape) == 3) and (len(weight.shape) == 2):
+        return np.sum(data * weight[:,np.newaxis,:], axis=polarization_axis) / norm
     else:
-        return data, flag, weight
+        raise RuntimeError("I don't know what to do with provided data and weight arrays with shapes {:} and {:}, respectively".format(data.shape, weight.shape))
 
+def contains_autocorrelations(ant1, ant2):
+    """
+    Test whether the list of antennas contain any autocorrelations.
 
-def get_crosscorrelation_mask(ant1, ant2):
+    Args:
+        ant1 (np.array int): antenna 1
+        ant2 (np.array int): antenna 2
+
+    Returns:
+        boolean: True if list contains autocorrelation pairs.
+    """
+    autocorrelation_mask = (ant1 == ant2)
+    return np.sum(autocorrelation_mask) > 0 
+
+def get_crosscorrelation_indexes(ant1, ant2):
 
     # index to cross-correlations
     xc = np.where(ant1 != ant2)[0]
 
     return xc
+
+def isdecreasing(chan_freq):
+    '''
+    Return true if channels are stored in decreasing frequency order, i.e., blueshifted to redshifted.
+
+    Args:
+        chan_freq (1D numpy array): the channel frequencies in Hz
+
+    Returns:
+        boolean : True if channels are stored in decreasing frequency order (preferred order).
+    '''
+    # check to make sure we're in blushifted - redshifted order, otherwise reverse channel order
+    nchan = len(chan_freq)
+    if nchan == 1:
+        return True
+
+    diff = np.diff(chan_freq)
+
+    if np.all(diff < 0):
+        return True # strictly decreasing
+    elif np.all(diff > 0):
+        return False # strictly increasing
+    else:
+        raise RuntimeError("chan_freq array is neither strictly decreasing nor strictly increasing, investigate what went wrong.")
+        
+def reverse_array(array, channel_axis=1):
+    """
+    If the channel frequencies are stored in an order different than the one we desire, we can reverse the axes.
+
+    Args:
+        array (np.array): the input array, could be chan_freq, data, flag, etc.
+        channel_axis (int): which axis is the channel axis?
+    
+    Returns:
+        np.array sorted
+    """
+    return np.flip(array, axis=channel_axis)
 
 
 def get_channel_sorted_data(filename, datadescid):
