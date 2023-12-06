a7961a2149692540620380b3926794f503ee9168
Ian Czekala <iancze@gmail.com>
2023-12-06 15:33:45 +0000

    moving towards updated build system.

--- setup.py
+++ setup.py
@@ -1,68 +1,8 @@
 import setuptools
-import os.path
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(here, rel_path), "r") as fp:
-        return fp.read()
-
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
-
-version = get_version("src/visread/__init__.py")
-
-EXTRA_REQUIRES = {}
-EXTRA_REQUIRES["casa"] = ["casatools", "casadata"]
-EXTRA_REQUIRES["test"] = EXTRA_REQUIRES["casa"] + ["pytest", "casatasks", "astropy"]
-EXTRA_REQUIRES["docs"] = EXTRA_REQUIRES["casa"] + [
-        "sphinx>=2.3.0",
-        "nbsphinx",
-        "sphinx_material",
-        "sphinx_copybutton",
-        "sphinx_rtd_theme",
-        "jupyter",
-        "jupytext",
-        "ipython!=8.7.0",  # broken version for syntax higlight https://github.com/spatialaudio/nbsphinx/issues/687
-        "astropy",
-        "casatasks",
-        "matplotlib",
-        "myst-nb",
-        "jupyter-cache"
-    ]
-
-EXTRA_REQUIRES["dev"] = (
-    EXTRA_REQUIRES["casa"] + EXTRA_REQUIRES["test"] + EXTRA_REQUIRES["docs"] + ["pylint", "black"]
-)
-
 
 setuptools.setup(
     name="visread",
-    version=version,
-    author="Ian Czekala",
-    author_email="iczekala@psu.edu",
-    description="Use CASA6 to read visibilities from Measurement Sets",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/MPoL-dev/visread",
-    install_requires=["numpy"],
-    extras_require=EXTRA_REQUIRES,
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires="~=3.6",  # CASA6 requirement
 )
