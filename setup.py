dcd6f13ad0080c0944b869a2f23d66de0b8a5d60
Ian Czekala <iancze@gmail.com>
2023-12-06 20:32:45 +0000

    using hatch.

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="visread",
-    url="https://github.com/MPoL-dev/visread",
-    packages=setuptools.find_packages("src"),
-    package_dir={"": "src"},
-)
