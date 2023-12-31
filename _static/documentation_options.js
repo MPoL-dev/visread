70ec89be1d3bcf54e1a524231b3a8bc8662a2800
iancze <iancze@users.noreply.github.com>
2023-12-31 15:16:45 +0000

    deploy: 56bfdefe7db6c41a5e5eb47cf019caffa496ad15

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g1049f6c',
+    VERSION: '0.1.dev1+g56bfdef',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
